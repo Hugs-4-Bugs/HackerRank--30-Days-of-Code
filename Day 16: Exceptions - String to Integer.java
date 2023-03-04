43b505fbacf403ab1c5e74cf4d821c97a6d89248
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-03-05 02:57:29 +0530

    Create Day 16: Exceptions - String to Integer.java

---
+++ Day 16: Exceptions - String to Integer.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+       Scanner in= new Scanner (System.in);
+String S = in.next();
+try{
+int a = Integer.parseInt(S);
+// }catch (Exception e) {
+System.out.println(a);
+}
+catch(Exception e) {
+System.out.println("Bad String");
+    }
+}
+}
