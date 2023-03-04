0293d7bc2d15400bdaa04cf9f5fca801b06cb672
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-03-05 02:49:54 +0530

    Create Day 8: Dictionaries and Maps.java

---
+++ Day 8: Dictionaries and Maps.java
@@ -0,0 +1,29 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+         Map<String,Integer> Contact =new HashMap<String,Integer>();
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            // System.out.println(n);
+            Contact.put(name,phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+             if(Contact.containsKey(s)){
+                System.out.println(s+"="+Contact.get(s));
+        }
+        else{
+            System.out.println("Not found");
+        }
+    }
+        in.close();
+    }
+}
