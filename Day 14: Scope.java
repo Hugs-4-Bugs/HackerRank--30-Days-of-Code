3029554a23c4ea4432700cd491b835954b634abc
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-03-05 02:54:42 +0530

    Create Day 14: Scope.java

---
+++ Day 14: Scope.java
@@ -0,0 +1,12 @@
+
+
+	// Add your code here
+ public Difference(int[]elements)
+    {
+        this.elements=elements;
+    }
+    void computeDifference(){
+        int n=elements.length;
+        Arrays.sort(elements);
+        maximumDifference=Math.abs(elements[n-1]-elements[0]);
+    }
